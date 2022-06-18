f935e79b49642083de5944fc8bba03d99a4544ad
bydariogamer <bydariogamer@inventati.org>
2022-06-18 20:26:06 +0200

    improve controls and reformat file

--- src/controls.py
+++ src/controls.py
@@ -1,8 +1,6 @@
+import time
+
 from .gmglobal import *
-
-
-def getcon(control, state):
-    pass  # This is called by doing: if getcon("up", "press"): Execute code
 
 
 state = {
@@ -16,12 +14,12 @@ state = {
 }
 
 
-def getcon(_control, _state):
+def get_control(_control, _state):
     state["keys"] = pg.key.get_pressed()
     # for i in config["key"]:
     for i in config["key"]:
         if state["keys"][config["key"][i]]:
-            if state[i]["held"] == False:
+            if not state[i]["held"]:
                 state[i]["press"] = True
             else:
                 state[i]["press"] = False
@@ -29,16 +27,15 @@ def getcon(_control, _state):
         else:
             state[i]["held"] = False
 
-    if state[_control][_state] == True:
+    if state[_control][_state]:
         return True
 
 
-def defControls():
+def def_controls():
     state["keys"] = pg.key.get_pressed()
-    # for i in config["key"]:
     for i in config["key"]:
         if state["keys"][config["key"][i]]:
-            if state[i]["held"] == False:
+            if not state[i]["held"]:
                 state[i]["press"] = True
             else:
                 state[i]["press"] = False
@@ -46,37 +43,37 @@ def defControls():
         else:
             state[i]["held"] = False
 
-import time
 
 class Keyboard:
-	def __init__(self):
-		self.keys = {}
+    def __init__(self):
+        self.keys = {}
 
-	def handle_event(self, event:int) -> None:
-		if event.type == pg.KEYDOWN:
-			self.keys[event.key] = time.time()
-		elif event.type == pg.KEYUP:
-			self.keys[event.key] = False
-	
-	def is_held(self, key:int) -> bool:
-		# check if the key is currently pressed
-		return key in self.keys and self.keys[key] is not False
+    def handle_event(self, event: pg.event) -> None:
+        if event.type == pg.KEYDOWN:
+            self.keys[event.key] = time.time()
+        elif event.type == pg.KEYUP:
+            self.keys[event.key] = False
 
-	def is_pressed(self, key:int) -> bool:
-		hold_time = self.hold_time(key)
-		if hold_time != None:
-			if hold_time <= 1/60:
-				return key in self.keys and self.keys[key] is not False
-	
-	def press_time(self, key:int) -> float:
-		# check the time the held key was pressed
-		if key not in self.keys or self.keys[key] is False:
-			return None
-		return self.keys[key]
-	
-	def hold_time(self, key:int) -> float:
-		# check for how long held key is pressed
-		if key in self.keys:
-			return time.time() - self.keys[key]
+    def is_held(self, key: int) -> bool:
+        """check if the key is currently pressed"""
+        return key in self.keys and self.keys[key]
+
+    def is_pressed(self, key: int) -> bool:
+        hold_time = self.hold_time(key)
+        if hold_time is not None:
+            if hold_time <= 1 / 60:
+                return key in self.keys and self.keys[key]
+
+    def press_time(self, key: int) -> [float]:
+        """check the time the held key was pressed"""
+        if key not in self.keys or not self.keys[key]:
+            return
+        return self.keys[key]
+
+    def hold_time(self, key: int) -> float:
+        """"check for how long held key is pressed"""
+        if key in self.keys:
+            return time.time() - self.keys[key]
+
 
 keyboard = Keyboard()
